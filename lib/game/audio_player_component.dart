89b71e391bce42f0be750abce65bc26871228cb6
John Thomas McDole <john@mcdole.org>
2022-08-27 09:57:15 -0700

    Fix crash on Windows + PlayData.isEqupped check
    - Windows doesn't work if we fail to load the background music.
      - Special case this and print a helpful developer log.
    - PlayerData.isEquipped check is checking function parameter against
      itself, vs against the player data.

--- lib/game/audio_player_component.dart
+++ lib/game/audio_player_component.dart
@@ -1,4 +1,5 @@
 import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:provider/provider.dart';
 
@@ -11,17 +12,26 @@ class AudioPlayerComponent extends Component with HasGameRef<SpacescapeGame> {
   Future<void>? onLoad() async {
     FlameAudio.bgm.initialize();
 
-    await FlameAudio.audioCache.loadAll([
-      'laser1.ogg',
-      'powerUp6.ogg',
-      '9. Space Invaders.wav',
-      'laserSmall_001.ogg'
-    ]);
+    await FlameAudio.audioCache
+        .loadAll(['laser1.ogg', 'powerUp6.ogg', 'laserSmall_001.ogg']);
+
+    try {
+      await FlameAudio.audioCache.load(
+        '9. Space Invaders.wav',
+      );
+    } catch (_) {
+      // ignore: avoid_print
+      print('Missing VOiD1 Gaming music pack: '
+          'https://void1gaming.itch.io/free-synthwave-music-pack '
+          'See assets/audio/README.md for more information.');
+    }
 
     return super.onLoad();
   }
 
   void playBgm(String filename) {
+    if (!FlameAudio.audioCache.loadedFiles.containsKey(filename)) return;
+
     if (gameRef.buildContext != null) {
       if (Provider.of<Settings>(gameRef.buildContext!, listen: false)
           .backgroundMusic) {
