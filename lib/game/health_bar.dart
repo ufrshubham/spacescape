f1c5ddad397ca844e9529197e3c3b73cc1e56451
Ryuzaki <ufrshubham@gmail.com>
2022-08-28 10:30:43 +0530

    Add health bar component

---
+++ lib/game/health_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+import 'player.dart';
+
+class HealthBar extends PositionComponent {
+  final Player player;
+
+  HealthBar({
+    required this.player,
+    super.position,
+    super.size,
+    super.scale,
+    super.angle,
+    super.anchor,
+    super.children,
+    super.priority,
+  }) {
+    positionType = PositionType.viewport;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    // Draws a rectangular health bar at top right corner.
+    canvas.drawRect(
+      Rect.fromLTWH(-2, 5, player.health.toDouble(), 20),
+      Paint()..color = Colors.blue,
+    );
+    super.render(canvas);
+  }
+}
